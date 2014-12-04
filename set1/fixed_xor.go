dd1fb4fc7f3d2e780fa76a785acbb1fc2b3df1f6
ss <Icelight@gmail.com>
2014-12-04 16:40:52 -0600

    Added solution for 1-2

---
+++ set1/fixed_xor.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+    "errors"
+    "encoding/hex"
+)
+
+//Assumes that input1 and input2 are already of equal length.
+func xorOverArray(input1 []byte, input2 []byte) []byte {
+    
+    outputBytes := make([]byte, len(input1))
+
+    for i, _ := range input1 {
+        if i > len(input2) {
+            break
+        }
+
+        outputBytes[i] = input1[i] ^ input2[i]
+    }
+
+    return outputBytes
+}
+
+
+
+func FixedXor(input1, input2 string) (string, error) {
+    if len(input1) != len(input2) {
+        return "", errors.New("Length of input strings were not the same")
+    }
+
+    input1HexBytes, err := hex.DecodeString(input1)
+
+    if err != nil {
+        return "", err
+    }
+
+    input2HexBytes, err := hex.DecodeString(input2)
+
+    if err != nil {
+        return "", err
+    }
+
+    xorBytes := xorOverArray(input1HexBytes, input2HexBytes)
+    outputString := hex.EncodeToString(xorBytes)
+
+    return outputString, nil
+}
---
+++ set1/fixed_xor_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+    "testing"
+)
+
+type testPairs struct {
+    input1 string
+    input2 string
+    expected string
+    shouldThrowError bool
+}
+
+var testCases = []testPairs {
+    {
+        input1: "1c0111001f010100061a024b53535009181c",
+        input2: "686974207468652062756c6c277320657965",
+        expected: "746865206b696420646f6e277420706c6179",
+        shouldThrowError: false,
+    },
+    {
+        input1: "1234567",
+        input2: "123456",
+        expected: "",
+        shouldThrowError: true,
+    },
+    {
+        input1: "123456789",
+        input2: "",
+        expected: "",
+        shouldThrowError: true,
+    },
+    {
+        input1: "123",
+        input2: "123456789ab",
+        expected: "",
+        shouldThrowError: true,
+    },
+    {
+        input1: "",
+        input2: "123",
+        expected: "",
+        shouldThrowError: true,
+    },
+}
+
+
+func TestFixedXor(t *testing.T) {
+
+    for _, testcase := range testCases {
+        actual, err := FixedXor(testcase.input1, testcase.input2)
+
+        if !testcase.shouldThrowError && err != nil {
+            t.Error("Error returned by FixedXor:", err.Error())
+        } else if testcase.shouldThrowError && err == nil {
+            t.Error("Expected error from FixedXor but did not get one!")
+        }
+
+        if actual != testcase.expected {
+            t.Error("Expected:", testcase.expected, "but got:", actual)
+        }
+    }
+}
